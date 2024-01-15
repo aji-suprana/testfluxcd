285cd7b939233437df2746a38d3b713119253391
aji-sup <ajiadiwenas@gmail.com>
2024-01-15 09:34:28 +0700

    add podinfo deployment

--- initialize.sh
+++ initialize.sh
@@ -1,11 +1,36 @@
  export GITHUB_USER=aji-suprana
- export GITHUB_TOKEN=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
+ export GITHUB_TOKEN=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
 
  flux bootstrap github \
   --components-extra=image-reflector-controller,image-automation-controller \
   --owner=$GITHUB_USER \
   --repository=testfluxcd \
   --branch=main \
-  --path=./ \
+  --path=clusters/my-cluster \
   --read-write-key \
   --personal
+
+cat <<EOF > ./podinfo-deployment.yaml
+apiVersion: apps/v1
+kind: Deployment
+metadata:
+  name: podinfo
+  namespace: default
+spec:
+  selector:
+    matchLabels:
+      app: podinfo
+  template:
+    metadata:
+      labels:
+        app: podinfo
+    spec:
+      containers:
+        - name: podinfod
+          image: ghcr.io/stefanprodan/podinfo:5.0.0
+          imagePullPolicy: IfNotPresent
+          ports:
+            - name: http
+              containerPort: 9898
+              protocol: TCP
+EOF
